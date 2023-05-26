8f26563dc61d3cadfb3464b570230ebde5d6b034
Sergey <kalashnikov.sergey01@gmail.com>
2023-05-27 00:30:34 +0300

    up

--- SQLScripts/create_first_user.sql
+++ SQLScripts/create_first_user.sql
@@ -2,7 +2,7 @@ use warehouse
 go 
 
 insert into dbo.Persons values
-( 'admin', 'vova', 'markus','admin', '1912-10-25' ,'password',0),
+--( 'admin', 'vova', 'markus','admin', '1912-10-25' ,'password',0),
 ( 'hradminn', 'valera', 'borisov','hradmin', '1962-10-25' ,'123456',0)
 go
 
@@ -13,8 +13,8 @@ alter server role securityadmin add member hradminn
 alter server role sysadmin add member hradminn
 
 
-select * from dbo.Persons
-delete dbo.Persons where dbo.Persons.[Login] = 'admin'
+--select * from dbo.Persons
+--delete dbo.Persons where dbo.Persons.[Login] = 'admin'
 
-drop trigger  add_login_and_role
-drop trigger delete_login_and_role
+--drop trigger  add_login_and_role
+--drop trigger delete_login_and_role
